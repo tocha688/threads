099c542487d61f53d169b2bac1fbde8b68e52419
tocha688 <tocha688@outlook.com>
2025-07-27 22:27:20 +0800

    优化导出

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./pLimit";
 export * from "./threads";
 export * from "./worker";
 export * from "./type";
-export * from "universal-message"
+export { MessageClient } from "universal-message";
+export type { MessageInfo } from "universal-message";
